48ac5d4a6251f2793b00f3d602deffcbc0e8c58f
seacore219 <seacore219@outlook.com>
2025-08-07 12:38:44 -0500

    try2

--- common/test_single_autosave.py
+++ common/test_single_autosave.py
@@ -1,216 +1,135 @@
-from __future__ import division  # has to be imported in every file
-#~ import matplotlib  # if from ssh
-#~ matplotlib.use('Agg')
-
-import warnings
-import tables
-from importlib import import_module
-import imp
-import ipdb  # prettier debugger
+from __future__ import division
 import os
 import sys
 sys.path.insert(1, "../")
 
-import sys
-if 'win_unicode_console' in sys.modules:
-    win_unicode_console = sys.modules['win_unicode_console']
-    win_unicode_console.disable()
-
 import utils
-
-# This assumes that you are in the folder "common"
 utils.initialise_backup(mount="../", dest="../backup")
 utils.backup(__file__)
 
-from utils.backup import dest_directory
+from importlib import import_module
 from common.stats import StatsCollection
 from common.sorn import Sorn
 import cPickle as pickle
 import gzip
 import numpy as np
-import matplotlib.pyplot as plt
-from datetime import datetime
-import shutil
+import tables
 
-# Get simulation number from environment variable
+# Get simulation number
 sim_number = int(os.environ.get('SORN_SIM_NUMBER', '1'))
-print "[INFO] Running simulation number:", sim_number
-
-# Set unique random seed for this simulation
 np.random.seed(sim_number * 12345)
-print "[INFO] Set random seed to:", sim_number * 12345
 
-def load_param_file(filename):
-    return imp.load_source('parameters', filename)
-
-# Start debugging mode when an error is raised
+# Silence debug errors
 def debugger(type,flag):
-    print 'In debugger!'
     pass
 np.seterrcall(debugger)
 np.seterr(all='call')
 
 # ========================================
-# PERIODIC H5 SAVING AND ROTATION
+# CHUNKED H5 SAVING
 # ========================================
-SAVE_INTERVAL = 100000  # Save H5 every 100k timesteps
-save_counter = [0]
+CHUNK_SIZE = 10000  # Save every 10k timesteps
+chunk_counter = [0]
 total_steps = [0]
+h5_chunks = []
 
-def rotate_h5_file(c):
-    """Save current H5 and mark for deletion"""
-    global save_counter
+def save_h5_chunk(c):
+    """Save current H5 as a chunk and start new one"""
+    global chunk_counter, h5_chunks
     
-    # Close current H5 file
-    try:
-        import tables
-        tables.file._open_files.close_all()
-    except:
-        pass
+    # Close current H5
+    tables.file._open_files.close_all()
     
-    # Current H5 file
     current_h5 = utils.logfilename("result.h5")
-    
     if os.path.exists(current_h5):
-        # Rename to chunk file
-        chunk_h5 = os.path.join(
-            c.logfilepath,
-            "result_sim%02d_chunk%04d.h5" % (sim_number, save_counter[0])
-        )
+        # Rename to chunk
+        chunk_name = "chunk_%03d.h5" % chunk_counter[0]
+        chunk_path = os.path.join(c.logfilepath, chunk_name)
+        os.rename(current_h5, chunk_path)
+        h5_chunks.append(chunk_path)
+        chunk_counter[0] += 1
         
-        # Move the file
-        shutil.move(current_h5, chunk_h5)
-        
-        # Mark for deletion after sync
-        marker_file = chunk_h5 + ".ready_to_delete"
-        open(marker_file, 'a').close()
-        
-        file_size = os.path.getsize(chunk_h5) / (1024.0 * 1024.0)
-        print "[ROTATE] Sim %d - Saved H5 chunk %d (%.1f MB) - ready for sync & delete" % (
-            sim_number, save_counter[0], file_size
-        )
-        
-        save_counter[0] += 1
-        
-        # Start new H5 file
-        return True
-    
-    return False
+        # Delete old chunks (keep last 5)
+        if len(h5_chunks) > 10:
+            for old_chunk in h5_chunks[:5]:
+                if os.path.exists(old_chunk):
+                    os.remove(old_chunk)
+                    # Mark for remote deletion
+                    open(old_chunk + ".deleted", 'w').close()
+            h5_chunks = h5_chunks[5:]
 
 # ========================================
 
-# Parameters are read from the second command line argument
-param = load_param_file(utils.param_file())
+# Load parameters - using import_module like original
+param = import_module(utils.param_file())
 experiment_module = import_module(param.c.experiment.module)
 experiment_name = param.c.experiment.name
-experiment = getattr(experiment_module,experiment_name)(param)
+experiment = getattr(experiment_module, experiment_name)(param)
 
 c = param.c
-c.logfilepath = utils.logfilename('')+'/'
+c.logfilepath = utils.logfilename('') + '/'
 
-(source,stats_single) = experiment.start()
+# Start experiment
+(source, stats_single) = experiment.start()
 
-sorn = Sorn(c,source)
-
-# Create a StatsCollection and fill it with methods for all statistics
+# Create SORN
+sorn = Sorn(c, source)
 stats = StatsCollection(sorn)
 stats.methods = stats_single
 sorn.stats = stats
 
-# ========================================
-# WRAP SIMULATION FOR PERIODIC SAVING
-# ========================================
+# Wrap simulation for chunked saving
 original_simulation = sorn.simulation
 
-def simulation_with_rotation(steps):
-    """Modified simulation that rotates H5 files periodically"""
+def chunked_simulation(steps):
+    """Run simulation with chunked H5 saving"""
     global total_steps
     
-    batch_size = 1000
-    steps_done = 0
-    
-    while steps_done < steps:
-        current_batch = min(batch_size, steps - steps_done)
+    remaining = steps
+    while remaining > 0:
+        batch = min(CHUNK_SIZE, remaining)
+        original_simulation(batch)
+        total_steps[0] += batch
+        remaining -= batch
         
-        # Run original simulation
-        original_simulation(current_batch)
-        
-        steps_done += current_batch
-        total_steps[0] += current_batch
-        
-        # Rotate H5 file every SAVE_INTERVAL steps
-        if total_steps[0] % SAVE_INTERVAL == 0:
-            print "[INFO] Rotating H5 file at step %d" % total_steps[0]
-            
-            # Save and close current H5
-            if hasattr(sorn.stats, 'dlog'):
-                sorn.stats.dlog.close()
-            
-            # Rotate the file
-            rotate_h5_file(c)
-            
-            # Reopen new H5
+        # Save chunk every 10k steps
+        if total_steps[0] % CHUNK_SIZE == 0:
+            # Close and rotate H5
+            sorn.stats.dlog.close()
+            save_h5_chunk(c)
+            # Reopen
             sorn.stats.dlog.set_handler('*', utils.StoreToH5, utils.logfilename("result.h5"))
-            
-            print "[INFO] New H5 file started"
-        
-        # Progress update
-        # Progress update every 10k timesteps
-        if total_steps[0] % 10000 == 0 and total_steps[0] > 0:
-            print "[Sim %d] Timesteps completed: %d" % (sim_number, total_steps[0])
 
-# Replace simulation method
-sorn.simulation = simulation_with_rotation
-print "[INFO] H5 rotation enabled for simulation %d" % sim_number
+sorn.simulation = chunked_simulation
 
-# ========================================
-
-# Datalog is used to store all results and parameters
-stats.dlog.set_handler('*',utils.StoreToH5,  utils.logfilename("result.h5"))
+# Setup stats
+stats.dlog.set_handler('*', utils.StoreToH5, utils.logfilename("result.h5"))
 stats.dlog.append('c', utils.unbunchify(c))
-stats.dlog.set_handler('*',utils.TextPrinter)
+stats.dlog.set_handler('*', utils.TextPrinter)
 
-# Final experimental preparations
+# Run experiment
 experiment.reset(sorn)
-
-# Start stats
 sorn.stats.start()
 sorn.stats.clear()
 
-# Run experiment once
-print "[INFO] Starting experiment..."
+# Run quietly - suppress percentage spam
+c.display = False
 pickle_objects = experiment.run(sorn)
 
-# Save final H5 chunk
-print "[INFO] Saving final H5 chunk"
-if hasattr(sorn.stats, 'dlog'):
-    sorn.stats.dlog.close()
-rotate_h5_file(c)
+# Save final chunk
+sorn.stats.dlog.close()
+save_h5_chunk(c)
 
-# Save sources etc (these are small)
+# Save other files
 for key in pickle_objects:
-    filename = os.path.join(c.logfilepath,"%s.pickle"%key)
-    topickle = pickle_objects[key]
-    pickle.dump(topickle, gzip.open(filename,"wb"), pickle.HIGHEST_PROTOCOL)
+    filename = os.path.join(c.logfilepath, "%s.pickle" % key)
+    pickle.dump(pickle_objects[key], gzip.open(filename, "wb"), pickle.HIGHEST_PROTOCOL)
 
-# Control: Firing-rate model
 if sorn.c.stats.control_rates:
     experiment.control_rates(sorn)
 
-# Report stats and close
 stats.single_report()
 stats.disable = True
+sorn.quicksave(filename=os.path.join(c.logfilepath, 'net.pickle'))
 
-# Save network final state
-sorn.quicksave(filename=os.path.join(c.logfilepath,'net.pickle'))
-
-# Create manifest
-manifest_file = os.path.join(c.logfilepath, "manifest_sim%02d.txt" % sim_number)
-with open(manifest_file, 'w') as f:
-    f.write("Simulation %d complete\n" % sim_number)
-    f.write("Total H5 chunks: %d\n" % save_counter[0])
-    f.write("Total timesteps: %d\n" % total_steps[0])
-
-print "[INFO] Simulation %d complete - %d H5 chunks created" % (sim_number, save_counter[0])
-
-# Note: Skip plotting since we have multiple H5 files
+print "[Sim %d] Complete - %d chunks saved" % (sim_number, chunk_counter[0])
